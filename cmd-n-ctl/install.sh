89de1ce6ff6cb784c276e7e438eeeab4ee97bc7a
Marion Anderson <lmanderson42@gmail.com>
2018-06-14 00:29:45 -0700

    installers now use virtual environments; verbosity improvements

--- cmd-n-ctl/install.sh
+++ cmd-n-ctl/install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # Install script for cmd-n-ctl part of mySatComm github project
+# Creates and installs directly into a virtual environment
 # Intended to be run on Raspberry Pi system
 
 # Script Setup
 # ============
-set -o errexit 
+set -o errexit
 set -o pipefail
+
 function errmsg() {
     echo -n "Something went wrong!"
     echo " Make sure your system is set up properly"
@@ -18,102 +20,97 @@ function helpmsg() {
     echo " in a virtual environment"
 }
 
+function errcheck() {
+  if ! [ $? ]; then
+    errmsg
+    exit 1
+  fi
+}
+
 # Script Execution
 # ================
-# Notify user script is running
-# -----------------------------
-if [ $# -gt 1 ]; then
-    echo "Error: too many arguments"
-    helpmsg
-    exit 1
-fi
-execstr="Running cmd-n-ctl install.sh"
-if [ $# -eq 1 ] && [ $1 = "--venv" ]; then
-    execstr+=" with python virtualenv"
-    echo "tmp file as installer flag" >> env_install
-elif [ $# -eq 1 ] && [ $1 = "-h" ]; then
-    helpmsg
-    exit 0
-elif [ $# -ne 0 ]; then
-    echo "Error: bad arguments"
-    helpmsg
-    exit 1
-fi
-echo "$execstr..."
-echo
+echo "Running cmd-n-ctl install.sh"
 
-# Install  hamlib
+# Install python3
 # ---------------
 echo
-echo "Checking for hamlib"
-dpkg -l libhamlib-doc libhamlib-dev libhamlib-utils &>/dev/null
+echo "Checking for python3"
+dpkg -l python3 &> /dev/null
 if [ $? ]; then
-    echo "hamlib alreading installed, moving on"
+  echo "python3 already installed, moving on"
 else
-    sudo apt install libhamlib-doc libhamlib-dev libhamlib-utils
+  sudo apt install python3 &> /dev/null
+  errcheck; echo "python3 installed!"
 fi
 
 # Install pip3
 # ------------
 echo
 echo "Checking for pip3..."
-dpkg -l python3-pip &>/dev/null
+dpkg -l python3-pip &> /dev/null
 if [ $? ]; then
-    echo "pip3 already installed, moving on"
+  echo "pip3 already installed, moving on"
 else
-    sudo apt install python3-pip
-    if ! [ $? ]; then
-        errmsg
-        exit 2
-    fi
+  sudo apt install python3-pip &> /dev/null
+  errcheck; echo "pip3 installed!"
+fi
+
+# Install  hamlib
+# ---------------
+echo
+echo "Checking for hamlib"
+dpkg -l libhamlib-doc libhamlib-dev libhamlib-utils &> /dev/null
+if [ $? ]; then
+  echo "hamlib alreading installed, moving on"
+else
+  sudo apt install libhamlib-doc libhamlib-dev libhamlib-utils &> /dev/null
+  errcheck; echo "hamlib installed!"
 fi
 
 # Install pigpio daemon
 # ---------------------
 echo
 echo "Checking for pigpio daemon..."
-dpkg -l pigpio &>/dev/null
+dpkg -l pigpio &> /dev/null
 if [ $? ]; then
-    echo "pigpio already installed, moving on"
+  echo "pigpio already installed, moving on"
 else
-    sudo apt install pigpio
-    if ! [ $? ]; then
-        errmsg
-        exit 3
-    fi
+  sudo apt install pigpio &> /dev/null
+  errcheck
+  echo "pigpio installed!"
 fi
 
-if  [ -e env_install ]; then
-    echo
-    echo "Installing virtualenv"
-    sudo apt install python3-venv
-    if ! [ $? ]; then
-        errmsg
-        exit 4
-    fi
-
-    # activating virtualenv
-    python3 -m venv env
-    source env/bin/activate
-    echo
-    echo "Virtual environment activated"
-fi
-
-# install SainSmart class and exception
+# Install & activate virtualenv
+# -----------------------------
 echo
+echo "Checking for virtualenv..."
+dpkg -l python3-virtualenv &> /dev/null
+if [ $? ]; then
+  echo "virtualenv already installed, moving on"
+else
+  sudo apt install python3-virtualenv
+  errcheck; echo "virtualenv installed!"
+fi
+# activating virtualenv
+echo "Creating virtual environment..."
+python3 -m venv satcomm
+source satcomm/bin/activate
+errcheck
+echo "Virtual environment activated!"
+echo "Installing python packages..."
+
+# Install python packages
+# -----------------------
 echo "Installing sainsmart-lib..."
-pip3 install -e sainsmart-lib/
-if ! [ $? ]; then
-    errmsg
-    exit 5
-fi
+pip3 install -e sainsmart-lib/; errcheck
+echo "Installing pyserial..."
+pip3 install pyserial; errcheck
 
+# Cleanup
+# -------
+deactivate
+echo
 echo "Installation Complete!"
-if [ -e env_install ]; then
-    rm env_install  # remove file flag
-    deactivate
-    echo
-    echo "Virtual environment stored in cmd-n-ctl/env/bin/activate"
-fi
-exit 0
+echo "Virtual environment stored in cmd-n-ctl/satcomm/bin/activate"
 
+exit 0
--- cmd-n-ctl/uninstall.sh
+++ cmd-n-ctl/uninstall.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 # Uninstall script for cmd-n-ctl part of mySatComm github project
-# Intended to be run on Raspberry Pi system
+# Intended to be run on a Raspberry Pi system
 
 # Script Setup
 # ============
-set -o errexit 
+set -o errexit
 set -o pipefail
 function errmsg() {
-    echo -n "Something went wrong!"
-    echo " Make sure your system is set up properly"
+  echo -n "Something went wrong!"
+  echo " Make sure your system is set up properly"
 }
 
 function helpmsg() {
-    echo "usage: uninstall.sh [--all]"
-    echo "Uninstalls antenna controller python libraries and scripts"
-    echo "  --all       Remove all packages, not just sainsmart-lib"
+  echo "usage: uninstall.sh [--all]"
+  echo "Uninstalls antenna controller python libraries and scripts"
+  echo "  --all       Remove all packages, not just sainsmart-lib"
+}
+
+function errcheck() {
+  if ! [ $? ]; then
+    errmsg
+    exit 1
+  fi
 }
 
 # Script Execution
@@ -22,31 +29,35 @@ function helpmsg() {
 # Notify user script is running
 # -----------------------------
 if [ $# -gt 1 ]; then
-    echo "Error: too many arguments"
-    helpmsg
-    exit 1
+  echo "Error: too many arguments"
+  helpmsg
+  exit 1
 elif [ $# -eq 1 ] && [ $1 = "-h" ]; then
-    helpmsg
-    exit 0
+  helpmsg
+  exit 0
 elif [ $# -ne 0 ]; then
-    echo "Error: bad argument"
-    helpmsg
-    exit 1
+  echo "Error: bad argument"
+  helpmsg
+  exit 1
 fi
 
-# remove SainSmart
+# Removing python packages
+# ------------------------
+# activate virtualenv
+source satcomm/bin/activate
+echo "Virtual environment activated..."
 echo "Removing sainsmart-lib..."
-if [ -e env/bin/activate ]; then
-    echo "qwer"
-    source env/bin/activate
-fi
-pip3 uninstall sainsmart
+pip3 uninstall sainsmart; errcheck; echo "sainsmart-lib uninstalled!"
+echo "Removing pyserial..."
+pip3 uninstall pyserial; errcheck; echo "pyserial uninstalled!"
 
-# remove else
-if [ $# -eq 1 ] && [ $1 -eq "--all" ]; then
-    sudo apt remove libhamlib-doc libhamlib-dev libhamlib-utils pigpio pip3 python3-venv
+# Remove other packages as well
+# -----------------------------
+if [ $1 -eq "--all" ]; then
+  echo "--all flag selected. Waiting 5 seconds in case of error"
+  sleep 5
+  sudo apt remove libhamlib-doc libhamlib-dev libhamlib-utils pigpio pip3 python3-venv
 fi
-
 
 echo "Removal Complete!"
 exit 0
