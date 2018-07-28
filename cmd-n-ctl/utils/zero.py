cf7747d5585c3f6d857d6f5846f01b3b85f45a8a
lmander42 <lmanderson42@gmail.com>
2018-07-28 03:09:13 -0700

    Added zeroing to interfaces

---
+++ cmd-n-ctl/utils/zero.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+CLI for zeroing the rotator.
+Intended for testing and setup uses.
+
+author: Marion Anderson
+date:   2018-07-28
+file:   zero.py
+"""
+from __future__ import absolute_import, print_function
+
+import os
+
+import serial
+
+from rotator import Rotator
+
+# Rotator setup
+rot = Rotator()
+rot.attach(23, 24, 27)  # from my soldershield design
+rot.zero()  # zero before doing anything
